17110b1117665214fc29bee44793e2b16111056e
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2018-09-07 00:31:46 -0400

    added basic mockup views

--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('login');
+  this.route('vendor', function() {});
+  this.route('non-vendor', function() {});
 });
 
 export default Router;
