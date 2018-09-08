9648b21db99df2523e3967b9904bc6109f479236
Kaiser Dandangi <kdandangi@freshbooks.com>
2018-09-08 12:20:34 -0400

    Added mocks to list views and added models

--- app/routes/non-vendor/index.js
+++ app/routes/non-vendor/index.js
@@ -1,4 +1,16 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+    model() {
+        return {
+            vendors: [
+                // {name: , status: }
+                {name: "Burger Shack", status: "Open"},
+                {name: "Woofle House", status: "Open"},
+                {name: "Tea & Friends", status: "Closed"}
+            ]
+        }
+    }
+
 });
--- app/routes/vendor/index.js
+++ app/routes/vendor/index.js
@@ -1,4 +1,22 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+    model() {
+        return {
+            items: [
+                // {name: , timer: }
+                /* timer should be a computed property that keeps incrementing*/
+                {name: "Burgers", timer: 123},
+                {name: "Rotis", timer: 77},
+                {name: "Lassi", timer: 987}
+            ]
+        }
+    }
+
+    actions: {
+        showTimer() {
+            alert("show timer");
+        }
+    }
 });
