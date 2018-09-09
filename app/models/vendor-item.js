1507a90877ff2a3f7e79f6c5e7accb634bb743b7
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2018-09-09 16:20:52 -0400

    removed uneeded attr

--- app/models/vendor-item.js
+++ app/models/vendor-item.js
@@ -4,8 +4,5 @@ import { computed } from '@ember/object'
 export default DS.Model.extend({
   name: DS.attr(),
   timerStartedAt: DS.attr(),
-  timerStopAt: DS.attr(),
-  timer: computed('timerStopAt', function() {
-    return moment(this.get('timerStopAt')).diff(moment()) / 1000;
-  })
+  timerStopAt: DS.attr()
 });
