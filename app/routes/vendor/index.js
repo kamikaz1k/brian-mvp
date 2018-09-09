2c847e21760532495922397b4c176a1425ee6d38
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2018-09-09 16:39:13 -0400

    added some formatting to vendor listview and login page

--- app/routes/vendor/index.js
+++ app/routes/vendor/index.js
@@ -5,19 +5,7 @@ export default Route.extend({
     model() {
         return {
             items: this.store.findAll('vendor-item')
-            // items: [
-            //     // {name: , timer: }
-            //     /* timer should be a computed property that keeps incrementing*/
-            //     {name: "Burgers", timer: 123},
-            //     {name: "Rotis", timer: 77},
-            //     {name: "Lassi", timer: 987}
-            // ]
-        }
-    },
-
-    actions: {
-        showTimer() {
-            alert("show timer");
         }
     }
+
 });
