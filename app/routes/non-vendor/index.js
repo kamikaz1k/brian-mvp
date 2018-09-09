75b3b19f6c811451b0766ca7c55e1431224901b4
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2018-09-09 16:57:03 -0400

    Added vendor model

--- app/routes/non-vendor/index.js
+++ app/routes/non-vendor/index.js
@@ -4,12 +4,17 @@ export default Route.extend({
 
     model() {
         return {
-            vendors: [
-                // {name: , status: }
+            vendors: this.store.findAll('vendor')
+        }
+    },
+
+    afterModel(model) {
+        if (model.vendors.length === 0) {
+            model.vendors = [
                 {name: "Burger Shack", status: "Open"},
                 {name: "Woofle House", status: "Open"},
                 {name: "Tea & Friends", status: "Closed"}
-            ]
+            ].map(v => this.store.createRecord('vendor', v));
         }
     }
 
