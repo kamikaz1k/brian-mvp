7ab124e6fc046d55201d242855677e70267c6a5e
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2018-09-09 23:21:56 -0400

    added the vendor items to the vendor show page with dummy subscribe buttons

--- app/routes/non-vendor/vendor/show.js
+++ app/routes/non-vendor/vendor/show.js
@@ -7,7 +7,15 @@ export default Route.extend({
       vendor: this.store.findRecord(
         'vendor',
         params.vendor_id,
-        { include: 'vendor-items' }
+        // { include: 'vendor-items' } -- can't figure out JSONAPI
+      ),
+      vendorItems: this.store.query(
+        'vendor-item',
+        {
+          filter: {
+            vendorId: params.vendor_id
+          }
+        }
       )
     }
   }
